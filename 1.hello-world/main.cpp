29e09a891beae14cb8bea18a4ef4fababafccebc
jhondev-code <jhondev.code@gmail.com>
2021-01-27 08:27:22 -0500

    Hola mundo

---
+++ 1.hello-world/main.cpp
@@ -0,0 +1,15 @@
+// Librerias de c++
+#include <iostream>	// proporciona funcionalidades de entrada y salida
+#include <cstdlib>	// proporciona constantes y clases de utilidad
+
+using namespace std;	// namespace usado para invocar las funciones de entrada y salida
+
+// funcion principal, el codigo a ejecutar se encuentra en aqui
+int main() {
+    // 'cout' c output, se usa para mostrar contenido en la pantalla
+    cout << "Hello world ;)" << endl;
+    // al ser main una funcion entera retorna un valor numerico
+    // EXIT_SUCCESS indica que el programa finalizo correctamente
+    return EXIT_SUCCESS;
+}
+
