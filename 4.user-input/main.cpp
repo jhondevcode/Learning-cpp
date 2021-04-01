17702330a38db09d0e6a96cb1bb43bcb92d5d737
jhondev-code <jhondev.code@gmail.com>
2021-04-01 11:12:18 -0500

    Entradas por el usuario

---
+++ 4.user-input/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+/* main function */
+int main(int argc, char const *argv[]) {
+    uint age = 0;
+    cout << "Ingresa tu edad: ";
+    cin >> age;
+    cout << "La edad ingresada es: " << age << endl;
+    return EXIT_SUCCESS;
+}
