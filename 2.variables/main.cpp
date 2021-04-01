fff8af943da41f8736132d7dc9f73004e8fe4512
jhondev-code <jhondev.code@gmail.com>
2021-04-01 10:58:35 -0500

    Varibles primitivas en c++

---
+++ 2.variables/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+/* main function */
+int main(int argc, char const *argv[]) {
+    char arroba = '@';
+    int age = 22;
+    float pi = 3.1416f;
+    double sun_distance = 150000000.1331555d;
+    bool open_door = true;
+    cout << "Arroba symbol: " << arroba << endl;
+    cout << "My age: " << age << endl;
+    cout << "Pi: " << pi << endl;
+    cout << "Sun distance: " << sun_distance << endl;
+    cout << "The door is open: " << open_door << endl;
+    return EXIT_SUCCESS;
+}
