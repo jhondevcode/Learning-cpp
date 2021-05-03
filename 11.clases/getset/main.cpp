54018234de0536895a5103b88f2cc63e5dd2f5a9
jhondev-code <jhondev.code@gmail.com>
2021-05-03 10:57:02 -0500

    Class get and set values

---
+++ 11.clases/getset/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class NotesBook {
+private:
+	string course_name;
+public:
+	void set_course_name(string name) {
+		this->course_name = name;
+	}
+	string get_course_name() const {
+		return this->course_name;
+	}
+	void show_message() const {
+		cout << "Welcome to notes book for " << this->get_course_name() << endl;
+	}
+};
+
+int main() {
+	NotesBook course;
+	string course_name;
+	cout << "Enter the course name: ";
+	fflush(stdout);
+	getline(cin, course_name);
+	fflush(stdin);
+	course.set_course_name(course_name);
+	course.show_message();
+	cout << "The name of course is: " << course.get_course_name() << endl;
+	cout << "Object position in memory: " << &course << endl;
+	course.n_curse();
+	cout << endl;
+	system("pause");
+	return EXIT_SUCCESS;
+}
